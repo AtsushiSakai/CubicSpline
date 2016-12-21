224992ef08a9d95f32e0bd0d4e60ab97757ff1ec
AtsushiSakai <asakai.amsl+github@gmail.com>
2016-12-20 16:34:35 -0800

    update Spline class for continious input x

--- PyCubicSpline/py_cubic_spline.py
+++ PyCubicSpline/py_cubic_spline.py
@@ -4,6 +4,7 @@ u"""
 Cubic Spline library
 
 author Atsushi Sakai
+
 license: MIT
 """
 import math
@@ -15,61 +16,90 @@ class Spline:
     Cubic Spline class
 
     usage:
-        spline=Spline(y)
+        spline=Spline(x,y)
         rx=np.arange(0,4,0.1)
         ry=[spline.calc(i) for i in rx]
     """
 
-    def __init__(self, y):
-        self.a, self.b, self.c, self.d, self.w = [], [], [], [], []
+    def __init__(self, x, y):
+        self.b, self.c, self.d, self.w = [], [], [], []
 
+        self.x = x
+        self.y = y
+
+        self.nx = len(x)  # dimension of x
+        h = np.diff(x)
+
+        # calc coefficient c
         self.a = [iy for iy in y]
 
-        for i in range(len(self.a)):
-            if i == 0:
-                self.c.append(0)
-            elif i == (len(self.a) - 1):
-                self.c.append(0)
-            else:
-                self.c.append(3.0 * (self.a[i - 1] - 2.0 * self.a[i] + self.a[i + 1]))
+        # calc coefficient c
+        A = self.__calc__A(h)
+        B = self.__calc__B(h)
+        self.c = np.linalg.solve(A, B)
+        #  print(self.c1)
 
-        for i in range(len(self.a)):
-            if i == 0:
-                self.w.append(0)
-            elif i == (len(self.a) - 1):
-                pass
-            else:
-                tmp = 4.0 - self.w[i - 1]
-                self.c[i] = (self.c[i] - self.c[i - 1]) / tmp
-                self.w.append(1.0 / tmp)
-
-        i = len(self.a) - 1
-        while(i > 0):
-            i -= 1
-            self.c[i] = self.c[i] - self.c[i + 1] * self.w[i]
-
-        for i in range(len(self.a)):
-            if i == (len(self.a) - 1):
-                self.b.append(0)
-                self.d.append(0)
-            else:
-                self.d.append((self.c[i + 1] - self.c[i]) / 3.0)
-                self.b.append(self.a[i + 1] - self.a[i] - self.c[i] - self.d[i])
+        # calc spline coefficient b and d
+        for i in range(self.nx - 1):
+            self.d.append((self.c[i + 1] - self.c[i]) / (3.0 * h[i]))
+            tb = (self.a[i + 1] - self.a[i]) / h[i] - h[i] * (self.c[i + 1] + 2.0 * self.c[i]) / 3.0
+            self.b.append(tb)
 
     def calc(self, t):
         u"""
         Calc position
+
+        if t is outside of the input x, return None
+
         """
 
-        j = int(math.floor(t))
-        if(j < 0):
-            j = 0
-        elif(j >= len(self.a)):
-            j = len(self.a) - 1
+        if t < self.x[0]:
+            return None
+        elif t > self.x[-1]:
+            return None
 
-        dt = t - j
-        result = self.a[j] + (self.b[j] + (self.c[j] + self.d[j] * dt) * dt) * dt
+        i = self.__search_index(t)
+        dx = t - self.x[i]
+        result = self.a[i] + self.b[i] * dx + self.c[i] * dx ** 2.0 + self.d[i] * dx ** 3.0
+
         return result
+
+    def __search_index(self, x):
+        u"""
+        search data segment index
+        """
+
+        for i in range(self.nx):
+            if self.x[i] - x > 0:
+                return i - 1
+
+    def __calc__A(self, h):
+        u"""
+        calc matrix A for spline coefficient c
+        """
+        A = np.zeros((self.nx, self.nx))
+        A[0, 0] = 1.0
+        for i in range(self.nx - 1):
+            if i is not self.nx - 2:
+                A[i + 1, i + 1] = 2.0 * (h[i] + h[i + 1])
+            A[i + 1, i] = h[i]
+            A[i, i + 1] = h[i]
+
+        A[0, 1] = 0.0
+        A[self.nx - 1, self.nx - 2] = 0.0
+        A[self.nx - 1, self.nx - 1] = 1.0
+        #  print(A)
+        return A
+
+    def __calc__B(self, h):
+        u"""
+        calc matrix B for spline coefficient c
+        """
+        B = np.zeros(self.nx)
+        for i in range(self.nx - 2):
+            B[i + 1] = 3.0 * (self.a[i + 2] - self.a[i + 1]) / h[i + 1] - 3.0 * (self.a[i + 1] - self.a[i]) / h[i]
+        #  print(B)
+        return B
 
     def calcd(self, t):
         u"""
@@ -123,15 +153,18 @@ def calc_yaw(s, sx, sy):
     return yaw
 
 
-if __name__ == '__main__':
+def test1():
     import matplotlib.pyplot as plt
     # input
-    x = [0, 1, 2, 3]
-    y = [2.7, 6, 5, 6.5]
+    #  x = [-1.0, 0.0, 1.0, 2.0, 3.0]
+    #  x = [-0.5, 0.0, 0.5, 1.0, 1.5]
+    x = [-2.5, 0.0, 2.5, 5.0, 7.5]
+    #  y = [3.2, 2.7, 6, 5, 6.5]
+    y = [0.7, -6, 5, 6.5, 0.0]
 
     # 3d spline interporation
-    spline = Spline(y)
-    rx = np.arange(0, 4, 0.1)
+    spline = Spline(x, y)
+    rx = np.arange(-2.0, 4, 0.01)
     ry = [spline.calc(i) for i in rx]
 
     plt.plot(x, y, "xb")
@@ -139,3 +172,7 @@ if __name__ == '__main__':
     plt.grid(True)
     plt.axis("equal")
     plt.show()
+
+
+if __name__ == '__main__':
+    test1()
